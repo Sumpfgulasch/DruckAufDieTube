82f2f11ebf94e3904c991e808e4c2422d8b3e921
Malte Decker <Malte.Decker@incari.com>
2022-11-26 14:12:55 +0100

    Added Game View Logic

---
+++ DruckAufDieTube/Assets/Scripts/UI/TimerUI.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+[RequireComponent(typeof(Stopwatch))]
+public class TimerUI : MonoBehaviour
+{
+
+    private UIDocument UI_Doc;
+    private Stopwatch stopwatch;
+
+    private Label timerLabel;
+
+    public void Start()
+    {
+        UI_Doc = this.transform.GetComponent<UIDocument>();
+        stopwatch = this.transform.GetComponent<Stopwatch>();
+
+        if (UI_Doc == null)
+        {
+            Debug.LogWarning("Couldnt find UI Document - Fatal Error");
+        }
+
+        var rootElement = UI_Doc.rootVisualElement;
+
+
+        timerLabel = rootElement.Q<Label>("TemporaryTimer");
+        timerLabel.text = stopwatch.Time.ToString();
+
+        stopwatch.OnTimeChange += OnStopwatchTimeChanged;
+    }
+
+    private void OnDestroy()
+    {
+        stopwatch.OnTimeChange -= OnStopwatchTimeChanged;
+    }
+
+    private void OnStopwatchTimeChanged(float time)
+    {
+        timerLabel.text = time.ToString();
+    }
+}
